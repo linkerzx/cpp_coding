99b4ac9104c65d1d92de9b68c98da97f886b5506
Julien Kervizic <julienk@julienk-mbp.local>
2015-12-18 15:22:30 +0000

    updating parse argument to return a vector<vstring>

--- main.cpp
+++ main.cpp
@@ -7,31 +7,35 @@
 typedef std::vector<std::string> vstring;
 using namespace std;
 
-vstring split_string(string &input_string)
+vstring split_string(string &input_string, const char delim)
 {
         vstring result;
         stringstream input_stream(input_string);
         string input_item;
-        while(getline(input_stream, input_item, '-'))
+        while(getline(input_stream, input_item, delim))
         {
                 result.push_back(input_item);
         };
 	return result;
 }
 
-string join(const vector<string>& vec, const char* delim)
+string join(const vstring& vec, const char* delim)
 {
     stringstream res;
     copy(vec.begin(), vec.end(), ostream_iterator<string>(res, delim));
     return res.str();
 }
 
-void parse_args(string &input_string)
+vector<vstring> parse_args(string &input_string)
 {
-	vstring result = split_string(input_string);
+	vstring result = split_string(input_string, '-');
+	vector<vstring> vresult; 
 	for(string str: result){
-		cout << str << endl;
+		vstring tmp_result = split_string(str, ' ');
+		vresult.push_back(tmp_result);
+		//cout << str << endl;
 	};
+	return vresult; 
 };
 
 int  main(int argc, char* argv[])
