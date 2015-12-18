a17cb37f9cde4b5ae14ea2a7d2d41216aa59d683
Julien Kervizic <julienk@julienk-mbp.local>
2015-12-18 22:59:51 +0000

    skeleton class for data file processor

--- processor.cpp
+++ processor.cpp
@@ -2,19 +2,33 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <tgmath.h>
 #include "string_utils.h"
 
 using namespace std;
 
-string row_processor(string &str_input, fileformat ff, int column_number)
+vstring row_processor(string &str_input, fileformat ff)
 {
 	// converts the row to a vector string by delimiter
 	vstring str_proc = split_string(str_input, get_delimiter(ff));
-	if(str_proc.size() >= column_number){
-		return str_proc[column_number];
-	}
-	return "0";
+	return str_proc;
 };
+
+class data_file {
+	public: 
+		string path; 
+		fileformat ff;
+		bool has_header; 
+		vstring header;
+	private: 	
+	data_file(){
+		//contructor 
+	};
+};
+
+// add a function generation for the data file class
+// add a sum/fn(x) on column operation for multiple columns at the same time
+// add a sorting operation
 
 int main() 
 {
@@ -25,17 +39,28 @@ int main()
 		cerr << "Help Me God, I can't open a simple file" << endl;
 		exit(1);
 	};
-	int i = 0;
-	double number = 0 ; 
+	int row_count = 0;
+	double sum = 0, sum_squared =0; 
+	outf << 
+		"row_count" << get_delimiter(CSV) << 
+		"sum" << get_delimiter(CSV) << 
+		"sum_squared" << 
+	endl;
 	while(!inf.eof()){
 		string str_sample_input; 
 		getline(inf, str_sample_input);
 		cout << str_sample_input << endl;
-		if(i > 0) {
-			number += stod(row_processor(str_sample_input, CSV, 2));
+		if(row_count > 0) {
+			double row_result = stod(row_processor(str_sample_input, CSV)[2]);
+			sum += row_result;
+			sum_squared += pow(row_result, 2);
 		}
-		i++;
-		outf << number << endl;
+		row_count++;
+		outf << 
+			row_count << get_delimiter(CSV) << 
+			sum << get_delimiter(CSV) << 
+			sum_squared <<  
+		endl;
 	};
 
 	return 0;	
